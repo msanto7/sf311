552e5f9fb2456e2f4db108e6be9c05deaa5a6e1c
MIKE <mike.santoro42@gmail.com>
2016-03-22 14:48:36 -0400

    angularjs expressions now displaying appropriately

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,19 +1,20 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'nvd3'])
+angular.module('myApp.view1', ['ngRoute'])
 
- .config(['$routeProvider', function($routeProvider) {
- $routeProvider.when('/view1', {
- templateUrl: 'view1/view1.html',
- controller: 'View1Ctrl'
- });
- }])
+    .config(['$routeProvider', function($routeProvider) {
+     $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+     });
+    }])
 
- .controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$scope',
+     function($scope) {
+      $scope.exampleData = "please show up";
 
-  $scope.exampleData = 'hello'
+     }]);
 
- }]);
 
 /*.controller('View1Ctrl', ['$scope', 'exampleData' {
 //var app = angular.module("nvd3TestApp", ['nvd3ChartDirectives']);
